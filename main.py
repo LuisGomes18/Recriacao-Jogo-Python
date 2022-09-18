2e49cc5922da198fbba0aeaaa30363e3249a5d04
LuisGomes18 <97010136+LuisGomes18@users.noreply.github.com>
2022-09-18 06:04:05 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,51 @@
+import random as rd
+
+vida = 0
+felicidade = 0
+biberaochao = 0
+biberao = 0
+
+def vida_bebe():
+    global vida
+    global felicidade
+    global biberao
+    vida = 12
+    felicidade = 6
+    print(f'Vida - {vida}')
+    print(f'Felicidade - {felicidade}')    
+    pais = str(input('Vai querer com seus pais? '))
+    if pais == "sim":
+        print('Voçe foi com os pais')
+        if felicidade > 12:
+            felicidade += 0
+            print(f'Vida - {vida}')
+            print(f'Felicidade - {felicidade}')
+        elif felicidade < 12:    
+            felicidade += 2
+            print(f'Vida - {vida}')
+            print(f'Felicidade - {felicidade}')
+    elif pais == "não":
+        print('Não foste com os pais')
+        felicidade += 0
+        print(f'Vida - {vida}')
+        print(f'Felicidade - {felicidade}')
+    else:
+        print('Resposta incorreta') 
+        felicidade += 0    
+        print(f'Vida - {vida}')
+        print(f'Felicidade - {felicidade}') 
+
+    print('Apanhar os biberões')
+    print('OBS: Neste caso ja que não tem interface e tudo gerado de forma random')
+    biberaochao = rd.randint(1, 2)
+    biberao = rd.randint(biberaochao, 9)
+    felicidade += biberao
+    felicidade -  biberao #temp
+    print(f'Vida - {vida}')
+    print(f'Felicidade - {felicidade}')
+
+
+comecar = str(input('Bora começar: '))
+if comecar == "sim":
+    print('Bora\n')
+    vida_bebe()
