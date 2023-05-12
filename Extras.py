dd72984a61b6a9d213c96ca8b8f11be5ad77a2c2
LuisGomes18 <luiscgomes0@gmail.com>
2023-05-12 22:22:55 +0100

    Add files via upload

--- Extras.py
+++ Extras.py
@@ -1,4 +1,5 @@
 from json import load
+from json import dump
 
 
 with open("Dados.json", encoding='utf-8') as dt:
@@ -7,8 +8,22 @@ amarelo = '\033[33m'
 cor_original = '\033[0;0m'
 negrito = '\033[1m'
 
+def Sexo():
+    sexo = str(input('''Insira o sexo da personagem (F - Feminino, M - Masculino, NB - Não Binario)
+-> '''))
+    sexo = sexo.lower()
+    if sexo == "f":
+        dados["sexo"] = sexo
+    elif sexo == "m":
+        dados["sexo"] = sexo
+    elif sexo == "nb":
+        dados["sexo"] = sexo
+    with open("Dados.json", "w") as dt:
+            dump(dados, dt)
 
 def Pontuacao():
+    with open("Dados.json", encoding='utf-8') as dt:
+        dados = load(dt)
     print(f'''\n\n{amarelo}{negrito}Pntuação:{cor_original}
 Vida: {dados["vida"]}
 Felicidade: {dados["felicidade"]}
--- Fase_Bebe.py
+++ Fase_Bebe.py
@@ -1,32 +1,37 @@
 from json import load
+from Extras import Sexo
 from Extras import Pontuacao
+from json import dump
+from time import sleep
 
 DEBUG = 1
-
 try:
     with open("Dados.json", encoding='utf-8') as dt:
         dados = load(dt)
         print('Dados Carregados')  #! tmp
 except FileNotFoundError:
     exit('Ficheiro não encontrado ou corrompido')
-felicidade = dados["felicidade"]
+felicidade = dados["felicidade_inicial"]
 
 def Fase_Bebe():
-    opcao_1 = str(input('Quer ir com os pais? (y/n)\n'))
-    if opcao_1 == "y":
+    global felicidade
+    Sexo()
+    opcao_1 = str(input('\nQuer ir com os pais? (s/n)\n'))
+    if opcao_1.lower() == "y":
         print('Voce foi com seu pais')
-        felicidade =+ 2
+        felicidade = felicidade + 2
         dados["felicidade"] = felicidade
         with open("Dados.json", "w") as dt:
-            json.dump(dados, dt)
+            dump(dados, dt)
         Pontuacao()
-    elif opcao_1 == "n":
+    elif opcao_1.lower() == "n":
         print('Voce não foi com seu pais')
         Pontuacao()
     else:
         print('Opção invalida')
+    sleep(2)
 
 if DEBUG == 1:
     Fase_Bebe()
 elif DEBUG == 0:
-    PRINT('FUNÇÃO DEBUG ESTA DESATIVADO')
+    print(' ')
