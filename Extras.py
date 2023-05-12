ffeae41a225ec044a9c95d5325476312de658406
LuisGomes18 <luiscgomes0@gmail.com>
2023-05-12 21:20:53 +0100

    Add files via upload
    -> A partir da versão 1.0, não haverá mais revisões do código.

--- Extras.py
+++ Extras.py
@@ -1,19 +1,15 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
 from json import load
-from sys import exit as ext
 
-try:
-    with open("Dados.json", encoding='utf-8') as dados_jg:
-        dados = load(dados_jg)
-except FileNotFoundError as e:
-    print(f'Erro: Arquivo Dados.json não encontrado')
-    ext()
 
-def Escolher_sexo_personagem():
-    global Sexo_Personagem
-    Sexo_Personagem = int(input('Escolha o sexo do seu personagem:\n 1 - Masculino\n 2 - Feminino\n 3- Não-Binarie\n--> '))
-    dados["dados"][0]["Sexo_Personagem"] = Sexo_Personagem
+with open("Dados.json", encoding='utf-8') as dt:
+    dados = load(dt)
+amarelo = '\033[33m'
+cor_original = '\033[0;0m'
+negrito = '\033[1m'
+
 
 def Pontuacao():
-    print('\033[33m' + f'\n\nFelicidade: {dados["dados"][0]["Felicidade"]}\nVida: {dados["dados"][0]["Vida"]}\n\n' + '\033[0m')
+    print(f'''\n\n{amarelo}{negrito}Pntuação:{cor_original}
+Vida: {dados["vida"]}
+Felicidade: {dados["felicidade"]}
+\n\n''')
--- Fase_Bebe.py
+++ Fase_Bebe.py
@@ -1,50 +1,32 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-from Extras import *
-from random import randint
-from time import sleep
 from json import load
-from sys import exit as ext
+from Extras import Pontuacao
+
+DEBUG = 1
 
 try:
-    with open("Dados.json", encoding='utf-8') as dados_jg:
-        dados = load(dados_jg)
+    with open("Dados.json", encoding='utf-8') as dt:
+        dados = load(dt)
+        print('Dados Carregados')  #! tmp
 except FileNotFoundError:
-    print('Erro: Arquivo Dados.json não encontrado')
-    ext()
+    exit('Ficheiro não encontrado ou corrompido')
+felicidade = dados["felicidade"]
 
 def Fase_Bebe():
-    Escolher_sexo_personagem()
-
-    sleep(5)
-    Ir_com_pais = str(input('\n\nVoçe que ir com seus pais? \n--> '))
-    if Ir_com_pais.lower() == 'sim':
-        print('Voçe foi com seu pais')
-        dados["dados"][0]["Felicidade"] += 2
-    elif Ir_com_pais.lower() not in ['nao', 'não']:
-        print('Voçe não foi com seu pais')
-        dados["dados"][0]["Felicidade"] += 0
+    opcao_1 = str(input('Quer ir com os pais? (y/n)\n'))
+    if opcao_1 == "y":
+        print('Voce foi com seu pais')
+        felicidade =+ 2
+        dados["felicidade"] = felicidade
+        with open("Dados.json", "w") as dt:
+            json.dump(dados, dt)
+        Pontuacao()
+    elif opcao_1 == "n":
+        print('Voce não foi com seu pais')
+        Pontuacao()
     else:
-        print('Resposta incorreta')
-    Pontuacao()
+        print('Opção invalida')
 
-    sleep(5)
-    print('Esta parte do código é feito por forma random já que não existe formato visual')
-    sleep(2)
-    Biberoes = randint(1, 9)
-    dados["dados"][0]["Felicidade"] = dados["dados"][0]["Felicidade"] + Biberoes // 6
-    print(f'Voçe consegiu "apanhar": {Biberoes} biberões')
-    Pontuacao()
-
-    sleep(5)
-    Ir_no_escorrega = str(input('Voçe quer ir no escorrega? \n--> '))
-    if Ir_no_escorrega.lower() == 'sim':
-        print('Voçe foi no escorrega')
-        dados["dados"][0]["Felicidade"] += 2
-    elif Ir_no_escorrega.lower() not in ['nao', 'não']:
-        print('Voçe não foi no escorrega')
-        dados["dados"][0]["Felicidade"] += 0
-    else:
-        print('Resposta incorreta')
-    Pontuacao()
-    dados["dados"][0]["Final_fase_bebe"] = 1
+if DEBUG == 1:
+    Fase_Bebe()
+elif DEBUG == 0:
+    PRINT('FUNÇÃO DEBUG ESTA DESATIVADO')
