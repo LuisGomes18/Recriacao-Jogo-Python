3c599bdd21959f2bcdc627f096255fcdfbab7590
LuisGomes18 <luiscgomes0@gmail.com>
2024-02-02 09:51:54 +0000

    Update do ficheiro lingua,py e dos ficheiros de texto

--- test/lingua.py
+++ test/lingua.py
@@ -3,10 +3,21 @@ import os
 
 diretorio_atual = os.getcwd()
 
-with open(rf'{diretorio_atual}\test\data\ing.json', 'r', encoding='utf-8') as fl_ing:
-    ing_language = json.load(fl_ing)
 
-with open(rf'{diretorio_atual}\test\data\pt-pt.json', 'r', encoding='utf-8') as fl_pt_pt:
-    pt_pt_language = json.load(fl_pt_pt)
+lingua = str(input('Lingua: (pt-pt ou ing) -> '))
+lingua.lower()
+match lingua:
+    case 'pt-pt':
+        with open(rf'{diretorio_atual}\test\data\pt-pt.json', 'r', encoding='utf-8') as fl_lingua:
+            language = json.load(fl_lingua)
+    case 'ing':
+        with open(rf'{diretorio_atual}\test\data\ing.json', 'r', encoding='utf-8') as fl_lingua:
+            language = json.load(fl_lingua)
+    case _:
+        print('Valor Invalido')
+        exit(1)
 
-print(ing_language, pt_pt_language)
+
+print('\n' + language['Phase_1'][0]['phrase_1'])
+print(language['Phase_1'][0]['phrase_2'])
+print(language['Phase_1'][0]['phrase_3'])
