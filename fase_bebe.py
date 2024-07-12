dba6b966cce98bd7cbb72bbf37c7e326ec247a22
LuisGomes18 <luiscgomes0@gmail.com>
2024-07-12 11:22:52 +0000

    Update da fase bebe

--- fase_bebe.py
+++ fase_bebe.py
@@ -4,50 +4,58 @@ from json_def import carregar_dados_player, guardar_dados_player
 
 
 def fase_bebe():
+    print('''
+    ███████╗ █████╗ ███████╗███████╗    ██████╗ ███████╗██████╗ ███████╗
+    ██╔════╝██╔══██╗██╔════╝██╔════╝    ██╔══██╗██╔════╝██╔══██╗██╔════╝
+    █████╗  ███████║███████╗█████╗      ██████╔╝█████╗  ██████╔╝█████╗  
+    ██╔══╝  ██╔══██║╚════██║██╔══╝      ██╔══██╗██╔══╝  ██╔══██╗██╔══╝  
+    ██║     ██║  ██║███████║███████╗    ██████╔╝███████╗██████╔╝███████╗
+    ╚═╝     ╚═╝  ╚═╝╚══════╝╚══════╝    ╚═════╝ ╚══════╝╚═════╝ ╚══════╝
+    ''')
     dados = carregar_dados_player()
 
-    concluida = dados["fase_crianca"][0]["concluida"]
+    concluida = dados['fase_crianca'][0]['concluida']
     if concluida:
         raise Exception('Config da fase criança precisa ser restaurado às definições de fábrica')
 
     escolha_sexo()
 
     pais = str(input('\nQuer ir com os pais (s/n)? ')).strip().lower()
-    while pais not in ["s", "n"]:
+    while pais not in ['s', 'n']:
         pais = str(input('Quer ir com os pais (s/n)? ')).strip().lower()
 
-    if pais == "s":
+    if pais == 's':
         print('Foste com os pais')
-    elif pais == "n":
+    elif pais == 'n':
         print('Foste sem os pais')
 
-    dados["fase_crianca"][0]["pais"] = pais
+    dados['fase_crianca'][0]['pais'] = pais
     guardar_dados_player(dados)
     pontuacao()
 
-    if pais == "s":
+    if pais == 's':
         biberao = randint(5, 9)
     else:
         biberao = randint(4, 9)
 
     print(f'\nApanhas-te {biberao} biberoes\n')
-    dados["fase_crianca"][0]["biberoes"] = biberao
+    dados['fase_crianca'][0]['biberoes'] = biberao
 
     guardar_dados_player(dados)
     pontuacao()
 
     escorrega = str(input('Quer ir ao escorrega (s/n)? ')).strip().lower()
-    while escorrega not in ["s", "n"]:
+    while escorrega not in ['s', 'n']:
         escorrega = str(input('Quer ir ao escorrega (s/n)? ')).strip().lower()
 
-    if escorrega == "s":
+    if escorrega == 's':
         print('Foste no escorrega')
     else:
         print('Não foste no escorrega')
 
-    dados["fase_crianca"][0]["escorrega"] = escorrega
+    dados['fase_crianca'][0]['escorrega'] = escorrega
     guardar_dados_player(dados)
     pontuacao()
 
-    dados["fase_crianca"][0]["concluida"] = True
+    dados['fase_crianca'][0]['concluida'] = True
     guardar_dados_player(dados)
