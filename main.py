71fbd81016877d9b1907b5695a708247dfa71e93
LuisGomes18 <luiscgomes0@gmail.com>
2024-09-04 19:27:18 +0100

    Update da importação dos modulos para modulos dentro das pastas

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from extra import welcome
-from fase_bebe import fase_bebe
-from fase_crianca import fase_crianca
+import sys
+import os
+from random import randint
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from extras.extra_def import welcome
+from niveis.fase_bebe import fase_bebe
+from niveis.fase_crianca import fase_crianca
 
 
 welcome()
