290ed20293915d87f7846207917e3056ee819482
LuisGomes18 <luiscgomes0@gmail.com>
2024-09-04 01:14:37 +0100

    Reformatação das definições

--- extras/extra_def.py
+++ extras/extra_def.py
@@ -1,14 +1,15 @@
 import platform
+import sys
 import os
-from json_def import  carregar_dados_player, guardar_dados_player
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from extras.json_def import carregar_dados_player, guardar_dados_player
 
 
 def limpar_terminal():
-    sistema = platform.system()
-    if sistema == "Windows":
-        os.system('cls')
-    else:
-        os.system('clear')
+    os.system("cls" if platform.system() == "Windows" else "clear")
 
 
 def welcome():
@@ -32,13 +33,11 @@ Linguagem de Programação Usadas: Python
 
 
 def pontuacao():
-    dados = carregar_dados_player()
+    dados = carregar_dados_player()["recursos"][0]
 
-    vida = dados["recursos"][0]["vida"]
-    dinheiro = dados["recursos"][0]["dinheiro"]
-    felicidade = dados["recursos"][0]["felicidade"]
+    vida, dinheiro, felicidade = (dados.get(i) for i in ("vida", "dinheiro", "felicidade"))
 
-    if vida is None or dinheiro is None or felicidade is None:
+    if any(i is None for i in (vida, dinheiro, felicidade)):
         raise Exception("Dados do player incorretos, corrija para prosseguir no jogo")
 
     print(f'\n\nVida: {vida}\nFelicidade: {felicidade}\nDinheiro: {dinheiro}\n\n')
@@ -53,19 +52,10 @@ def escolha_sexo():
     dados = carregar_dados_player()
 
     # Solicita ao jogador que escolha o sexo da personagem
-    sexo = str(input('Escolha o sexo da personagem (F- Feminino, M - Masculino): ')).strip().lower()
-
-    # Verifica se o sexo escolhido   valido
-    while sexo not in ["f", "m"]:
-        # Se n o for, solicita novamente
-        sexo = str(input('Escolha o sexo da personagem (F- Feminino, M - Masculino): ')).strip().lower()
-
-    # Se o sexo for feminino, imprime uma mensagem de confirma o
-    if sexo == "f":
-        print('E uma menina\n')
-    # Se o sexo for masculino, imprime uma mensagem de confirma o
-    else:
-        print('E um menino\n')
+    while True:
+        sexo = input('Escolha o sexo da personagem (F- Feminino, M - Masculino): ').strip().lower()
+        if sexo in ("f", "m"):
+            break
 
     # Adiciona o sexo escolhido aos dados do jogador
     dados["recursos"][0]["sexo"] = sexo
--- extras/json_def.py
+++ extras/json_def.py
@@ -15,14 +15,10 @@ def carregar_dados_player():
         Exception: Se ocorrer qualquer outro erro durante o processo de carregamento.
     '''
     try:
-        os.makedirs('data', exist_ok=True)
         with open('../data/player.json', 'r', encoding='utf-8') as f:
-            data = json.load(f)
-            if not isinstance(data, dict) or data is None:
-                raise TypeError('O conteúdo do arquivo "player.json" deve ser um dicionário não nulo.')
-            return data
-    except FileNotFoundError as exc:
-        raise FileNotFoundError('O arquivo "player.json" não foi encontrado.') from exc
+            return json.load(f)
+    except FileNotFoundError:
+        raise FileNotFoundError('O arquivo "player.json" não foi encontrado.')
     except json.JSONDecodeError as exc:
         raise json.JSONDecodeError('Houve um erro de decodificação JSON ao carregar os dados.') from exc
     except Exception as exc:
@@ -47,11 +43,7 @@ def guardar_dados_player(dados: dict):
 
     try:
         os.makedirs('data', exist_ok=True)
-        with open('../data/player.json', 'w', encoding='utf-8') as f:
-            json.dump(dados, f, ensure_ascii=False, indent=4)
+        with open('../data/player.json', 'wb') as f:
+            json.dump(dados, f, ensure_ascii=False, separators=(',', ':'))
     except FileNotFoundError as fnf_error:
         raise FileNotFoundError(f'O arquivo "player.json" não pôde ser encontrado: {fnf_error}') from fnf_error
-    except json.JSONDecodeError as json_error:
-        raise json.JSONDecodeError(f'Houve um erro ao decodificar JSON: {json_error}') from json_error
-    except Exception as error:
-        raise Exception(f'Ocorreu um erro ao salvar os dados do jogador: {error}') from error
